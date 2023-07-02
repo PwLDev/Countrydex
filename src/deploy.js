1466783e8765fe6f5a544b359acb1a65ee75167e
PWL / SmartElectron <pgibran09@gmail.com>
2023-07-01 18:40:01 -0700

    Fixed name issue

--- src/deploy.js
+++ src/deploy.js
@@ -17,7 +17,7 @@ export function publishCommands(io) {
         const { default: command } = await import(`./commands/${file}`);
 
         if (!command.data) return;
-        command.data.name.replace("countrydex", DataConfig["dexName"]).replace("Countrydex", DataConfig["dexName"]).replace("countryballs", DataConfig["countryballsName"] + "s")
+        command.data.name = command.data.name.toLowerCase()
         availableCommands.unshift(command.data)
     });
 
